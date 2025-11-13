61412023dc6afd3840733bc66b462345dc0e62f7
homulily85 <157974079+homulily85@users.noreply.github.com>
2025-11-13 21:42:39 +0700

    feat: implement sign language processing features

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+
+from routes import sign_language_processor
 from routes import user
+
 app = FastAPI()
 
 app.include_router(user.router)
+app.include_router(sign_language_processor.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
