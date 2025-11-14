fc376ca1d8a8c7e69084b3705f7c8ebd54f1ec01
homulily85 <157974079+homulily85@users.noreply.github.com>
2025-11-14 22:51:01 +0700

    feat: update Dl model

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
 from routes import sign_language_processor
-from routes import user
+# from routes import user
 
 app = FastAPI()
 
-app.include_router(user.router)
+# app.include_router(user.router)
 app.include_router(sign_language_processor.router)
 
 @app.get("/")
