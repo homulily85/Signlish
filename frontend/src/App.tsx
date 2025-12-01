26d1f807c81647cf0af249dce5019279774d80dd
Lambda <tiendat.workspace@gmail.com>
2025-12-01 23:36:08 +0700

    feat(ui): add login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,35 @@
-import './App.css'
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "@/components/mode-toggle"
+import LoginPage from "@/pages/auth/Login"
+import SignupPage from "./pages/auth/Register"
+import PasswordResetPage from "./pages/auth/PasswordReset"
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="min-h-screen p-4">
-        <div className="flex justify-end mb-4">
-          <ModeToggle />
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <div className="min-h-screen bg-background text-foreground flex flex-col">
+          <main className="flex-1">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <header className="w-full flex justify-end p-4">
+                    <ModeToggle />
+                  </header>
+                }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<SignupPage />} />
+              <Route path="/password_reset" element={<PasswordResetPage />} />
+            </Routes>
+          </main>
         </div>
-        <main>
-          <div className="h-screen flex items-center justify-center">
-            <h1 className="text-3xl font-bold underline">
-              Hello, Vite + React!
-            </h1>
-          </div>
-        </main>
-      </div>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
- 
+
 export default App
