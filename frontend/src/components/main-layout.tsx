57d7dc0859c7bb8d9671bde5192d6a35325e50bc
vietanh711 <vietanh122005@gmail.com>
2025-12-17 02:09:02 +0700

    delete header

--- frontend/src/components/main-layout.tsx
+++ frontend/src/components/main-layout.tsx
@@ -7,7 +7,6 @@ export default function MainLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className="flex h-screen">
       <div className="flex flex-1 flex-col">
-        <Header />
         <main className={cn("flex-1 overflow-y-auto p-4")}>{children}</main>
       </div>
     </div>
